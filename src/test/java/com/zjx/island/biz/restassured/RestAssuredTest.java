9e5c6fd25461e88c4614c995516a87275f7db24c
zjxht62 <zhao_ji_xiang@126.com>
2021-01-02 09:46:48 +0800

    添加restassured用例

--- src/test/java/com/zjx/island/biz/restassured/RestAssuredTest.java
+++ src/test/java/com/zjx/island/biz/restassured/RestAssuredTest.java
@@ -2,11 +2,13 @@ package com.zjx.island.biz.restassured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -20,7 +22,6 @@ import static org.hamcrest.Matchers.*;
  */
 public class RestAssuredTest {
     public static Headers HEADERS;
-    public int anInt  = 1;
 
     @BeforeClass
     public static void addHeader() {
@@ -36,9 +37,35 @@ public class RestAssuredTest {
         Response response = given().body("{\"data\":{\"loginName\":\"13241336315\",\"loginPassword\":\"7cc2cf8a95f80a8ea500ff997f9623e4\"},\"host\":\"https://sit-www.trustlife.com\",\"environmentCode\":0}")
 //            .contentType(ContentType.JSON)
             .headers(HEADERS)
-            .when().post("http://10.0.0.3:8341/eTrust/microLogin");
-        System.out.println(response.body().print());
-        response.then().assertThat().body("success", equalTo(true));
+            .when().post("http://10.0.0.3:8341/eTrust/microLogin")
+
+            .then()
+            //assertThat是语法糖
+            .assertThat().statusCode(200)
+            .assertThat().body("success", equalTo(true))
+            .assertThat().body("resultMsg", equalTo("操作成功"))
+
+//              提取单个值
+//            .extract().path();
+
+            //提取多个值,可以返回response
+        .extract()
+            .response();
+        Boolean resultMsg = response.path("success");
+        String errorCode = response.path("errorCode");
+        String data = response.path("data");
+        System.out.println(resultMsg);
+        System.out.println(errorCode);
+        System.out.println(data);
+
+
+        //单独使用JsonPath解析返回
+        JsonPath jsonPath = new JsonPath(data);
+        //比如出返回报文里的authToken
+        String authToken = jsonPath.getString("data.token.authToken");
+//        List<Integer> winnerIds = jsonPath.get("winners.winnderId");
+        System.out.println(authToken);
+
 
 
     }
